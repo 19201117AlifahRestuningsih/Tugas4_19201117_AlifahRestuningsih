735dc7f7266a2c98adedd419a055a9e4ff9a386a
19201117_Alifah Restuningsih <90738802+19201117AlifahRestuningsih@users.noreply.github.com>
2021-10-05 17:15:48 +0700

    Add files via upload

---
+++ v_regis2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Halaman Regristrasi</title>
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/styleregis.css">
+    </head>
+   
+    <body>
+        <div class="container">
+          <h1>Registrasi</h1>
+            <form>
+                <label>Username</label><br>
+                <input type="text"><br>
+				<label>Email</label><br>
+                <input type="text"><br>
+                <label>Password</label><br>
+                <input type="password"><br>
+                <button>Sign Up</button>
+            </form>
+        </div>     
+    </body>
+</html>
